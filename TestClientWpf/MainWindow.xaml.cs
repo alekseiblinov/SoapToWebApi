592c0fd1d74345e64a2caeb16138bb4619e7b599
Administrator <alex_m_b@mail.ru>
2024-08-30 11:09:48 +0300

    The test client has been redesigned from .NET 8 on .Net Framework 4.6.2.

---
+++ TestClientWpf/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using TestClientWpf.SoapWebService;
+
+namespace TestClientWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdCallSoapServiceMethod_Click(object sender, RoutedEventArgs e)
+        {
+            bool webserverConnectionTestResult;
+
+            //Создание объекта веб-службы.
+            using (Service webServiceInstance = new Service())
+            {
+                //Вызов метода проверки соединения с БД.
+                webserverConnectionTestResult = webServiceInstance.WebserverConnectionTest();
+            }
+
+            var messageText = webserverConnectionTestResult ? 
+                "Connections successful." : 
+                "Connection failed";
+            MessageBox.Show(messageText);
+        }
+
+        private void cmdCallWebApiServiceMethod_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
